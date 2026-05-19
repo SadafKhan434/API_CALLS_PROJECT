2916313826b7f7d35ab6575dd1d84616604cb19b
SadafKhan434 <sadafkhan2192@gmail.com>
2026-05-19 14:00:10 +0530

    message

---
+++ src/Components/Movie.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Movie=(props)=>{
+    return(
+    <li className={classes.movie}>
+    <h2>{props.title}</h2>
+    <h3>{props.releaseDate}</h3>
+</li>
+    )
+}
+export default Movie;
---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,16 @@
+import Movie from'./Movie';
+import classes from'./MovieList.module.css';
+const MovieList=(props)=>{
+    return(
+        <ul className={classes['movies-List']}>
+            {props.movies.map((movie)=>(
+               <Movie
+               title={movie.title} 
+               releaseDate={movie.release}
+               openingText={movie.openingText}
+               />
+            ))}
+            </ul>
+    )
+}
+export default MovieList;
