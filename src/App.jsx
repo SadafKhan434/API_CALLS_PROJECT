43be2f4ac7050a7ca458aecef583b9165661ca4e
SadafKhan434 <sadafkhan2192@gmail.com>
2026-05-26 19:55:40 +0530

    context

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App(){
     setIsLoading(true);
     setError(null)
     try{
-      const response =  await  fetch('https://swapi.info/api/films');
+      const response =  await  fetch('https://react-http-420ff-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
  
   if(!response.ok){
     throw new Error("Something went wrong!");
@@ -21,15 +21,20 @@ function App(){
   }
     const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData)=>{
-      return{
-        id: movieData.episode_id,
-      title: movieData.title,
-      openingText: movieData.opening_crawl,
-      releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    const loadedMovies=[];
+
+    for (const key in data){
+
+      loadedMovies.push({
+        id:key,
+        title:data[key].title,
+        openingText:data[key].openingText,
+        releaseDate:data[key].releaseDate
+      });
+    }
+
+    
+    setMovies(loadedMovies);
      
   } catch(error){
   setError(error.message)
@@ -42,20 +47,33 @@ useEffect(()=>{
    },[fetchMovieHandler])
 
 
+ async function addMovieHandler(movie){
+   const response= await fetch('https://react-http-420ff-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',{
+    method:'POST',
+    body:'JSON.stringify(movie)',
+    headers:{
+      'Content-Type': 'application/json'
+    }
+    })
+    const data = await response.json()
+    console.log(data);
+}
+
+
 let content=<p>Found no movies.</p>
 if(movies.length>0){
   content=<MovieList movies={movies}/>
 }
  
     
-  const addMovieHandler = useCallback((newMovie) => {
-  const movieWithId = {
-    ...newMovie,
-    id: Date.now().toString(), 
-  };
+ // const addMovieHandler = useCallback((newMovie) => {
+//  const movieWithId = {
+   // ...newMovie,
+    //id: Date.now().toString(), 
+  //};//
 
-  setMovies((prevMovies) => [movieWithId, ...prevMovies]); 
-}, []);
+//  setMovies((prevMovies) => [movieWithId, ...prevMovies]); 
+ //}, []);//
 
 
 return(
